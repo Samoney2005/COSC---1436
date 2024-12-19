f4c221de74ac45a47714a9c18743e66e5b26d70d
Samoney2005 <samone.a.cook-1@ou.edu>
2024-12-18 23:01:13 -0600

    first commit

---
+++ Homework/homework1.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout << "apples";
+    cout << "oranges";
+
+    cout << "apple";
+    cout << endl; // Fixed: Replaced "endl" as text with actual line break
+    cout << "kiwi";
+
+    cout << "a\nb\nc";
+
+    cout << 4 + 2 * 3;
+
+    cout << 49 % 5;
+
+    cout << (3 + 4) * (8 % 5);
+
+    cout << "cats/dogs";
+
+    cout << "programming is easy" << endl;
+
+    cout << "5 * 2 + 6 * 3";
+
+    cout << "a" << endl << "z";
+
+    int A;
+    A = 15;
+    A = A * 3;
+    cout << A;
+
+    A = -4;
+    A = A + 4;
+    cout << A; // Fixed: Changed "cout << 'A'" to "cout << A" to print the value of A
+
+    int X = (5 % 2) + (15 / 3);
+    cout << X * 2;
+
+    int Orange;
+    Orange = 5 + 7 * 2 - 3;
+    cout << Orange;
+
+    int Year = 2020;
+    int New = 10;  // Fixed: Corrected assignment to "New" without reassigning "Year"
+    cout << "Year = " << New << endl;
+
+    int T1 = 70;
+    int T2 = 75;
+    cout << T2 - T1 * 0.5 << " degrees";
+
+    cout << 1 << "2" << 3 << "4" << 5;
+
+    A = -16;
+    A = A * -0.25;
+    cout << A;
+
+    int B = 4;
+    cout << B * (B + B) * 3 - B + (B % 3);
+
+    cout << "Spiderman\n";
+
+    return 0;
+}
---
+++ Homework/homework2.cpp
@@ -0,0 +1,45 @@
+// Samone Cook
+// Homework #2
+// This program performs three tasks:
+// 1. Prints ASCII art of an elephant.
+// 2. Calculates the sum and product of three numbers provided by the user.
+// 3. Calculates the area of a circle, volume of a sphere, and surface area of a sphere given a radius input by the user.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Program 1
+    cout << "Program 1 - ASCII elephant\n";
+    cout << "           /  \\~~~/  \\    \n";
+    cout << "     ,----(     ..    )    \n";
+    cout << "    /      \\__     __/      \n";
+    cout << "   /|         (\\  |( \n";
+    cout << "  ^ \\   /___\\  /\\ |   hjw  \n";
+    cout << "     |__|   |__|-\"    `97  \n\n";
+    // Program 2
+    cout << "Program 2\n";
+    float N1, N2;
+    cout << "This program calculates the sum and product of three numbers.\n";
+    cout << "Enter the first number: ";
+    cin >> N1;
+    cout << "Enter the second number: ";
+    cin >> N2;
+    cout << "The sum is " << N1 + N2 << endl << endl;
+    // Program 3
+    const float Pi = 3.1416;
+    cout << "Program 3\n";
+    cout << "This calculates the area of a circle, volume of a sphere, and surface area of a sphere.\n";
+    cout << "Enter a radius: ";
+    float Radius;
+    cin >> Radius;
+    float CircleArea = Pi * Radius * Radius;
+    float SphereVolume = (4.0 / 3.0) * Pi * Radius * Radius * Radius;
+    float SphereSurfaceArea = 4 * Pi * Radius * Radius;
+    cout << "Area of a circle is " << CircleArea << " square inches\n";
+    cout << "Volume of a sphere is " << SphereVolume << " cubic inches\n";
+    cout << "Surface area of a sphere is " << SphereSurfaceArea << " square inches\n";
+
+
+    return 0;
+}
